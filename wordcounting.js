e51e99adbc353e2c63cafabd96a9fbbbdbe3763c
Stella Ma <sm873x@gmail.com>
2016-07-11 14:37:30 -0400

    JS - ran jshint to fix log variableand function variables

--- wordcounting.js
+++ wordcounting.js
@@ -1,10 +1,11 @@
 //WORD COUNTING - HOMEWORK 13
 (function() {
-var sonnets = 'THE SONNETS by William Shakespeare 1 From fairest creatures we desire increase, That thereby beauty\'s rose might never die, But as the riper should by time decease, His tender heir might bear his memory: But thou contracted to thine own bright eyes, Feed\'st thy light\'s flame with self-substantial fuel, Making a famine where abundance lies, Thy self thy foe, to thy sweet self too cruel: Thou that art now the world\'s fresh ornament, And only herald to the gaudy spring, Within thine own bud buriest thy content, And tender churl mak\'st waste in niggarding: Pity the world, or else this glutton be, To eat the world\'s due, by the grave and thee.'
+    'use strict';
+var sonnets = 'THE SONNETS by William Shakespeare 1 From fairest creatures we desire increase, That thereby beauty\'s rose might never die, But as the riper should by time decease, His tender heir might bear his memory: But thou contracted to thine own bright eyes, Feed\'st thy light\'s flame with self-substantial fuel, Making a famine where abundance lies, Thy self thy foe, to thy sweet self too cruel: Thou that art now the world\'s fresh ornament, And only herald to the gaudy spring, Within thine own bud buriest thy content, And tender churl mak\'st waste in niggarding: Pity the world, or else this glutton be, To eat the world\'s due, by the grave and thee.';
 
 
 function wordCount (text) {
-    var separatedText = sonnets.split(' ');
+    var separatedText = text.split(' ');
 
     var onlyWords = separatedText.filter(function(fullWords) {
         return fullWords.length >= 2;
@@ -24,8 +25,8 @@ function wordCount (text) {
     return onlyWords;
 }
 
-var wordCount = wordCount(sonnets);
-console.log(wordCount);
+var wordLog = wordCount(sonnets);
+console.log(wordLog);
 
 
 
