2d4aa2cd376ba83b28fd9e833423fdbf6e3aeeaf
Stella Ma <sm873x@gmail.com>
2016-07-11 14:31:57 -0400

    HTML - took away text, didn't need. JS - removed variables outside of function, restrained all to only one function, added filter to remove single character words

--- wordcounting.js
+++ wordcounting.js
@@ -1,17 +1,32 @@
 //WORD COUNTING - HOMEWORK 13
-
 (function() {
+var sonnets = 'THE SONNETS by William Shakespeare 1 From fairest creatures we desire increase, That thereby beauty\'s rose might never die, But as the riper should by time decease, His tender heir might bear his memory: But thou contracted to thine own bright eyes, Feed\'st thy light\'s flame with self-substantial fuel, Making a famine where abundance lies, Thy self thy foe, to thy sweet self too cruel: Thou that art now the world\'s fresh ornament, And only herald to the gaudy spring, Within thine own bud buriest thy content, And tender churl mak\'st waste in niggarding: Pity the world, or else this glutton be, To eat the world\'s due, by the grave and thee.'
 
-var text = 'THE SONNETS by William Shakespeare 1 From fairest creatures we desire increase, That thereby beauty\'s rose might never die, But as the riper should by time decease, His tender heir might bear his memory: But thou contracted to thine own bright eyes, Feed\'st thy light\'s flame with self-substantial fuel, Making a famine where abundance lies, Thy self thy foe, to thy sweet self too cruel: Thou that art now the world\'s fresh ornament, And only herald to the gaudy spring, Within thine own bud buriest thy content, And tender churl mak\'st waste in niggarding: Pity the world, or else this glutton be, To eat the world\'s due, by the grave and thee.'
-var splitText = [];
 
-function seperateWords (text, separator) {
-    var seperatedText = text.split(' ');
-    splitText.push(seperatedText);
+function separateWords (text, separator) {
+    var separatedText = text.split(' ');
+
+    var onlyWords = separatedText.filter(function(fullWords) {
+        return fullWords.length >= 2;
+    });
+
+    // onlyWords.forEach(function(word, i) {
+    //     var eachWord = (onlyWord[i]) === (onlyWords[])
+    // });
+
+
+    // var wordList = onlyWords.map(function(word) {
+    //     onlyWords.forEach(function(word) {
+    //         var word = (onlyWords[i])
+    //     })
+    // });
+
+    return onlyWords;
 }
 
-seperateWords(text);
-console.log(splitText);
+var separateWords = separateWords(sonnets);
+console.log(separateWords);
+
 
 
 })();
