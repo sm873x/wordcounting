3cfa0b10d1b18f5f5946fcbfac803ab8ef911d58
Stella Ma <sm873x@gmail.com>
2016-07-11 18:08:14 -0400

    Cleaned up spacing, deleted sonnets text

--- wordcounting.js
+++ wordcounting.js
@@ -1,12 +1,9 @@
-//WORD COUNTING - HOMEWORK 13
+
 (function() {
     'use strict';
-var sonnets = 'THE SONNETS by William Shakespeare 1 From fairest creatures we desire increase, That thereby beauty\'s rose might never die, But as the riper should by time decease, His tender heir might bear his memory: But thou contracted to thine own bright eyes, Feed\'st thy light\'s flame with self-substantial fuel, Making a famine where abundance lies, Thy self thy foe, to thy sweet self too cruel: Thou that art now the world\'s fresh ornament, And only herald to the gaudy spring, Within thine own bud buriest thy content, And tender churl mak\'st waste in niggarding: Pity the world, or else this glutton be, To eat the world\'s due, by the grave and thee.';
-
 
 function wordCount (text) {
-    var noPunctuation = text.replace(/\.|\'|\:|\,/g, '');
-    // console.log(noPunctuation);
+    var noPunctuation = text.replace(/\'s|'\.|\'|\:|\,|\*|\!|\@|\%|\^|\(|\)|\+|\=|\~|\`|\?|\>|\/|\_/g, '');
 
     var separatedText = noPunctuation.toLowerCase().split(' ');
 
@@ -17,15 +14,13 @@ function wordCount (text) {
     var wordCount = {};
 
     onlyWords.forEach(function(word) {
-
         wordCount[word] = wordCount[word] ? wordCount[word]+1 : 1;
-
     });
 
     return wordCount;
 }
 
-var wordLog = wordCount(sonnets);
+var wordLog = wordCount(text);
 console.log(wordLog);
 
 })();
